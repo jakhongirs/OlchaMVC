0fc2c59ffe5f92d535059b58b04bbd01694b40be
activus <r.ubaydullayev@uic.group>
2022-11-06 18:44:16 +0500

    feat: create, read, delete and update product

--- OlchaMVC/Controllers/ProductController.cs
+++ OlchaMVC/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using OlchaMVC.Models;
+using System.Text;
 
 namespace OlchaMVC.Controllers
 {
@@ -13,6 +15,8 @@ namespace OlchaMVC.Controllers
             client = new HttpClient();
             client.BaseAddress = baseAddress;
         }
+
+        /*ALL PRODUCTS*/
         public IActionResult Index()
         {
             List<ProductViewModel>modelList=new List<ProductViewModel>();
@@ -20,8 +24,94 @@ namespace OlchaMVC.Controllers
             if (response.IsSuccessStatusCode)
             {
                 string data = response.Content.ReadAsStringAsync().Result;
+                modelList = JsonConvert.DeserializeObject<List<ProductViewModel>>(data);
+            }
+            return View(modelList);
+        }
+
+        /*CREATE PRODUCT*/
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ProductViewModel model)
+        {
+            string data = JsonConvert.SerializeObject(model);
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = client.PostAsync(client.BaseAddress + "/product", content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
             }
             return View();
+        }
+
+        /*UPDATE PRODUCT*/
+        public IActionResult Edit(int id)
+        {
+            ProductViewModel model = new ProductViewModel();
+            HttpResponseMessage response = client.GetAsync(client.BaseAddress + "/product/"+id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                model = JsonConvert.DeserializeObject<ProductViewModel>(data);
+            }
+            return View("Create", model);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(ProductViewModel model)
+        {
+            string data = JsonConvert.SerializeObject(model);
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = client.PutAsync(client.BaseAddress + "/product/" + model.Id, content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Create", model);
+        }
+
+        /*DELETE PRODUCT*/
+        public IActionResult Delete(int id)
+        {
+            ProductViewModel model = new ProductViewModel();
+            HttpResponseMessage response = client.GetAsync(client.BaseAddress + "/product/" + id).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                model = JsonConvert.DeserializeObject<ProductViewModel>(data);
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(ProductViewModel model)
+        {
+            HttpResponseMessage response = client.DeleteAsync(client.BaseAddress + "/product/" + model.Id).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Delete", model);
+        }
+
+        /*DETAIL PRODUCT*/
+        public IActionResult Details(int id)
+        {
+            ProductViewModel model = new ProductViewModel();
+            HttpResponseMessage response = client.GetAsync(client.BaseAddress + "/product/" + id).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                model = JsonConvert.DeserializeObject<ProductViewModel>(data);
+            }
+            return View(model);
         }
     }
 }
