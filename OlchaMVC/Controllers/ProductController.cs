c35364526c663426a48c7b7062bdcdb8d9512eff
activus <r.ubaydullayev@uic.group>
2022-11-06 17:43:02 +0500

    Add project files.

---
+++ OlchaMVC/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OlchaMVC.Models;
+
+namespace OlchaMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        Uri baseAddress = new Uri("http://ec2-54-161-44-249.compute-1.amazonaws.com/api");
+        HttpClient client;
+
+        public ProductController()
+        {
+            client = new HttpClient();
+            client.BaseAddress = baseAddress;
+        }
+        public IActionResult Index()
+        {
+            List<ProductViewModel>modelList=new List<ProductViewModel>();
+            HttpResponseMessage response = client.GetAsync(client.BaseAddress + "/product").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+            }
+            return View();
+        }
+    }
+}
---
+++ OlchaMVC/Models/ProductViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace OlchaMVC.Models
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Price { get; set; } = string.Empty;
+    }
+}
